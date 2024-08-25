3ff8914cbd259a0b93a964033b5e898a8b53e0b1
Nick Garbalau <nick.g@worldviewltd.com>
2024-08-25 16:04:03 +0200

    fix: update code

--- README.md
+++ README.md
@@ -1,17 +1,4 @@
 ### Quick Start
 
-Run API
-
-```console
-python main.py
-```
-
-### Endpoints
-
-```python
-@app.route('/bookmarks', methods=['GET'])
-@app.route('/bookmarks', methods=['POST'])
-@app.route('/bookmarks/<string:id>', methods=['GET'])
-@app.route('/bookmarks/<string:id>', methods=['PUT'])
-@app.route('/bookmarks/<string:id>', methods=['DELETE'])
-```
+### Ref
+https://docs.python-telegram-bot.org/en/v21.4/
--- main.py
+++ main.py
@@ -1,102 +1,142 @@
-import logging
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-from uuid import uuid4
-from utils import load_bookmarks, save_bookmarks, bookmark_is_valid
+import replicate
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-app = Flask(__name__)
-CORS(app)
+REPLICATE_API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
+TELEGRAM_API_KEY = os.getenv('TELEGRAM_API_KEY')
+BOT_NAME = os.getenv('BOT_NAME')
 
-app.config.from_mapping(
-    BOOKMARKS_FILE='./bookmarks.json',
-    PORT=5000
-)
+print(f"TELEGRAM_API_KEY: {TELEGRAM_API_KEY}, REPLICATE_API_TOKEN: {REPLICATE_API_TOKEN}, BOT_NAME: {BOT_NAME}, ")
 
-logging.basicConfig(level=logging.INFO)
+# COMMANDS
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    intro_text = (
+        "Welcome to Vonder!\n\n"
+        "You're interacting with an AI, not a human.\n"
+        "While the AI aims to generate accurate content, the results are fully computer-generated and may not always be perfect.\n\n"
+        "You can use the following commands to interact with me:\n\n"
+        "/start — Get familiar with Vonder\n"
+        "/help — Get an example of how to construct prompts\n"
+        "/enhance — Tips to enhance your prompts\n"
+        "/generate [prompt] — Generate an image based on your prompt\n\n"
+        "Invite friends & get perks 🎁"
+    )
+    await update.message.reply_text(intro_text)
 
+async def generate_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    command = update.message.text
+    if len(command.split()) < 2:
+        await update.message.reply_text("Please provide a prompt after the /generate command.", reply_to_message_id=update.message.message_id)
+        return
 
-@app.route('/bookmarks', methods=['GET'])
-def get_bookmarks():
+    _, after_command = command.split(' ', 1)
+    await update.message.reply_text("Generating...", reply_to_message_id=update.message.message_id)
+    
     try:
-        data = load_bookmarks(app.config['BOOKMARKS_FILE'])
-        if len(data["bookmarks"]) == 0:
-            return jsonify({"bookmarks": []}), 200
-        return jsonify(data)
-    except Exception:
-        return jsonify({'error': 'Unable to access bookmarks.'}), 500
-
-
-@app.route('/bookmarks/<string:id>', methods=['GET'])
-def get_bookmark_by_id(id):
-    try:
-        data = load_bookmarks(app.config['BOOKMARKS_FILE'])
-        bookmark = next((b for b in data["bookmarks"] if b["id"] == id), None)
-        if bookmark:
-            return jsonify(bookmark)
+        image_url = await generate_image(after_command)
+        print("generate_command image_url:", image_url)
+        if image_url:
+            await update.message.reply_photo(
+                photo=image_url, 
+                caption="Done, enjoy 🤗",
+                reply_to_message_id=update.message.message_id
+            )
         else:
-            jsonify({'error': 'No bookmark with this id exists.'}), 404
+            await update.message.reply_text("Sorry, I couldn't generate the image.", reply_to_message_id=update.message.message_id)
+    except Exception as e:
+        await update.message.reply_text(f"An error occurred: {str(e)}. Please try a different prompt.", reply_to_message_id=update.message.message_id)
 
-    except Exception:
-        return jsonify({'error': 'Unable to access bookmarks.'}), 500
+async def ehance_prompt_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    intro_text = (
+        "Few tips on better prompts:\n\n"
+        "1. Talk to the Vonder like it is a human\n"
+        "2. Set the stage and provide context\n"
+        "3. Provide details, like 'happy' or 'dynamic shot'\n"
+        "4. Don't be afraid to experiment\n\n"
+        "Good luck 🩵"
+    )
+    await update.message.reply_text(intro_text)
 
-
-@app.route('/bookmarks', methods=['POST'])
-def create_bookmark():
-    if not request.is_json:
-        return jsonify({'error': 'Invalid content type.'}), 415
-    bookmark = request.get_json()
-    if not bookmark_is_valid(bookmark):
-        return jsonify({'error': 'Invalid bookmark data.'}), 400
-
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    demo_prompt = "an astronaut riding a horse on mars, hd, dramatic lighting"
+    await update.message.reply_text("Let me generate an image to show how prompts work.", reply_to_message_id=update.message.message_id)
+    
     try:
-        data = load_bookmarks(app.config['BOOKMARKS_FILE'])
-        print(data)
-        new_bookmark = {
-            "title": bookmark["title"],
-            "url": bookmark["url"],
-            "id": str(uuid4())
+        image_url = await generate_image(demo_prompt)
+        if image_url:
+            await update.message.reply_photo(
+                photo=image_url, 
+                caption="To achieve this, I used:\n\n\"an astronaut riding a horse on mars, hd, dramatic lighting\"",
+                reply_to_message_id=update.message.message_id
+            )            
+            await update.message.reply_text("Now it's your turn!\n\n Use /generate followed by your prompt to create your own image.", reply_to_message_id=update.message.message_id)
+        else:
+            await update.message.reply_text("Sorry, I couldn't generate the example image.", reply_to_message_id=update.message.message_id)
+    except Exception as e:
+        await update.message.reply_text(f"An error occurred: {str(e)}. Please try again later.", reply_to_message_id=update.message.message_id)
+
+async def handle_response(text: str) -> str:
+    processed: str = text.lower()
+    print('handle_response text:', processed)
+
+    if 'hello' in processed:
+        return f'Hey!'
+    return 'Sorry, I am not able to communicate (yet)'
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+
+    print(f'user ({update.message.chat.id}) in {message_type}: {text}')
+
+    if message_type in ['group', 'supergroup']:
+        if BOT_NAME.lower() in text.lower():
+            new_text: str = text.replace(BOT_NAME, '').strip()
+            response: str = await handle_response(new_text)
+        else:
+            return
+    else:
+        response: str = await handle_response(text)
+
+    print(f'vonder: {response}')
+    await update.message.reply_text(response)
+
+async def unknown_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Sorry, I didn't understand that command. Please use one of the available commands from the list.")
+
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'update {update} caused error: {context.error}')
+
+# GENERATE IMAGE
+async def generate_image(prompt: str):
+    output = replicate.run('black-forest-labs/flux-dev', input={
+            "prompt": prompt,
+            "guidance": 3.5,
+            "num_outputs": 1,
+            "aspect_ratio": "1:1",
+            "output_format": "webp",
+            "output_quality": 80,
+            "prompt_strength": 0.8,
+            "num_inference_steps": 50
         }
-        data["bookmarks"].append(new_bookmark)
-        save_bookmarks(data, app.config['BOOKMARKS_FILE'])
-        return jsonify(new_bookmark), 201
-    except Exception:
-        return jsonify({'error': 'Could not save bookmark.'}), 500
-
-
-@app.route('/bookmarks/<string:id>', methods=['PUT'])
-def update_bookmark(id):
-    if not request.is_json:
-        return jsonify({'error': 'Invalid content type.'}), 415
-    updated_data = request.get_json()
-    if not bookmark_is_valid(updated_data):
-        return jsonify({'error': 'Invalid bookmark data.'}), 400
-
-    try:
-        data = load_bookmarks(app.config['BOOKMARKS_FILE'])
-        bookmark = next((b for b in data["bookmarks"] if b["id"] == id), None)
-        if not bookmark:
-            return jsonify({'error': 'No bookmark with this id exists.'}), 404
-        bookmark.update(updated_data)
-        save_bookmarks(data, app.config['BOOKMARKS_FILE'])
-        return jsonify(bookmark)
-    except Exception:
-        return jsonify({'error': 'Unable to update bookmark.'}), 500
-
-
-@app.route('/bookmarks/<string:id>', methods=['DELETE'])
-def delete_bookmark(id):
-    try:
-        data = load_bookmarks(app.config['BOOKMARKS_FILE'])
-        bookmarks = data["bookmarks"]
-        if id not in [b["id"] for b in bookmarks]:
-            return jsonify({'error': 'No bookmark with this id exists.'}), 404
-        data["bookmarks"] = [b for b in bookmarks if b["id"] != id]
-        save_bookmarks(data, app.config['BOOKMARKS_FILE'])
-        return jsonify({'success': f'Bookmark with id {id} deleted.'})
-    except Exception:
-        return jsonify({'error': 'Unable to delete bookmark.'}), 500
-
+    )
+    return output[0]
 
 if __name__ == '__main__':
-    app.run(port=app.config['PORT'])
+    print('starting Vonder...')
+    app = Application.builder().token(TELEGRAM_API_KEY).build()
+
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('generate', generate_command))
+    app.add_handler(CommandHandler('enhance', ehance_prompt_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    app.add_handler(MessageHandler(filters.COMMAND, unknown_command))
+    app.add_error_handler(error)
+
+    print('polling...')
+    app.run_polling(poll_interval=3)
